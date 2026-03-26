1b5d4d906140d8921f7dec077a6557e91fc953e1
Victor Chinukwue <Vickysaintbrown02@gmail.com>
2026-03-26 16:35:10 +0100

    Add redirect aliases for missing routes

---
+++ src/app/blog/07-launch-week-checklist-and-first-7-days-image-prompts/page.tsx
@@ -0,0 +1,5 @@
+import { permanentRedirect } from "next/navigation";
+
+export default function LaunchWeekImagePromptsRedirectPage() {
+  permanentRedirect("/blog/07-launch-week-checklist-and-first-7-days");
+}
---
+++ src/app/website-redesign-lagos/page.tsx
@@ -0,0 +1,5 @@
+import { permanentRedirect } from "next/navigation";
+
+export default function WebsiteRedesignLagosPage() {
+  permanentRedirect("/services/website-redesign");
+}
