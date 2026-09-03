e6ee8e935c675a1fccf8fab827f20015f6f51323
Victorious <vickysaintbrown02@gmail.com>
2026-09-03 10:36:48 +0100

    Clean Stage 12 shell imports

--- src/components/whatsapp/WhatsAppShell.tsx
+++ src/components/whatsapp/WhatsAppShell.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useId, useState } from "react";
-import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import type { ReactNode } from "react";
